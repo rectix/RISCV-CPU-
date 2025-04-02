585c46c66ff1a8ae03c638d50deb22b868e27a95
rectix <76615506+rectix@users.noreply.github.com>
2025-04-02 22:44:33 +0200

    Add files via upload

---
+++ cpu_core_macros.svh
@@ -0,0 +1,9 @@
+`timescale 1ns / 1ps
+
+
+
+
+`define XLEN          32                 
+`define ILEN          32   
+`define PC_INIT       32'h0000_0000  
+`define NOP_INSTRUCT  32'h0000_0000          
